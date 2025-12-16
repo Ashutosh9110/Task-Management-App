b1f16073e6ff9c1f90ce04ac286e67327f142ee1
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-16 17:35:17 +0530

    added logic for token sign and verifying token

--- Backend/src/utils/jwt.ts
+++ Backend/src/utils/jwt.ts
@@ -1,2 +1,17 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
 
+const JWT_SECRET = process.env.JWT_SECRET!
+
+export interface TokenPayload extends JwtPayload {
+  userId: string
+}
+
+export const signToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
+}
+
+export const verifyToken = (token: string): TokenPayload => {
+  return jwt.verify(token, JWT_SECRET) as TokenPayload
+}
