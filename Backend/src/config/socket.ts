2dec2f4377255bf2f53eceabf8f6bcbd7e8caa2d
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-17 18:16:29 +0530

    added logic for socket connection and socket.authMiddleware

---
+++ Backend/src/config/socket.ts
@@ -0,0 +1,23 @@
+import { Server } from "socket.io"
+import http from "http"
+import { socketAuthMiddleware } from "../middlewares/socketAuth.middleware.js"
+
+export let io: Server
+
+export const initSocket = (server: http.Server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true
+    }
+  })
+
+  io.use(socketAuthMiddleware)
+
+  io.on("connection", (socket) => {
+    const userId = socket.data.userId
+    socket.join(userId)
+
+    console.log(`User connected: ${userId}`)
+  })
+}
