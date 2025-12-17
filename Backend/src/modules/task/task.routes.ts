4f0d7e78f8dbc18eac3cb4e3ca96e4fac4689bb1
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-17 18:10:07 +0530

    added task routes

---
+++ Backend/src/modules/task/task.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express"
+import { TaskController } from "./task.controller.js"
+import { authMiddleware } from "../../middlewares/auth.middleware.js"
+
+const router = Router()
+const controller = new TaskController()
+
+router.use(authMiddleware)
+
+router.post("/", controller.create)
+router.get("/", controller.getAll)
+router.get("/:id", controller.getById)
+router.put("/:id", controller.update)
+router.delete("/:id", controller.delete)
+
+export default router
