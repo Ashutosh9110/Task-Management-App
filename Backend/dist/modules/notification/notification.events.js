e5c6b9c39a2f59479db775b4f68b53c95aa3f0bc
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-22 15:20:07 +0530

    Remove dist from version control and ignore build output

--- Backend/dist/modules/notification/notification.events.js
+++
@@ -1,4 +0,0 @@
-import { io } from "../../config/socket.js";
-export const emitNotification = (userId, notification) => {
-    io.to(userId).emit("notification:new", notification);
-};
--- Backend/dist/server.js
+++
@@ -1,9 +0,0 @@
-import http from "http";
-import app from "./app.js";
-import { initSocket } from "./config/socket.js";
-const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
-initSocket(server);
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
